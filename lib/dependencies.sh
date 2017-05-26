2eb450fa1f0d98a250b63de81836a603a8f42635
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-25 23:38:10 -0400

    we

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -77,7 +77,12 @@ npm_node_modules() {
     else
       echo "Installing node modules (package.json)"
     fi
+    
     npm install --unsafe-perm --userconfig $build_dir/.npmrc --cache $build_dir/.npm 2>&1
+     echo "pushing clamav db cvds"
+    wget -O $build_dir/clamav/share/clamav/main.cvd http://database.clamav.net/main.cvd
+    wget -O $build_dir/clamav/share/clamav/daily.cvd http://database.clamav.net/daily.cvd
+     wget -O $build_dir/clamav/share/clamav/bytecode.cvd http://database.clamav.net/bytecode.cvd
   else
     echo "Skipping (no package.json)"
   fi
