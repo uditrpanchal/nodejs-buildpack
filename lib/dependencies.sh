d803c15f7e6c3656b98e1f6423e7060e530a4f4e
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-25 23:55:16 -0400

    qwee

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -79,9 +79,9 @@ npm_node_modules() {
     fi
     
     npm install --unsafe-perm --userconfig $build_dir/.npmrc --cache $build_dir/.npm 2>&1
-    wget -O $build_dir/clamav/share/clamav/main.cvd http://database.clamav.net/main.cvd
-    wget -O $build_dir/clamav/share/clamav/daily.cvd http://database.clamav.net/daily.cvd
-    wget -O $build_dir/clamav/share/clamav/bytecode.cvd http://database.clamav.net/bytecode.cvd
+    curl -o $build_dir/clamav/share/clamav/main.cvd http://database.clamav.net/main.cvd
+    curl -o $build_dir/clamav/share/clamav/daily.cvd http://database.clamav.net/daily.cvd
+    curl -o $build_dir/clamav/share/clamav/bytecode.cvd http://database.clamav.net/bytecode.cvd
   else
     echo "Skipping (no package.json)"
   fi
