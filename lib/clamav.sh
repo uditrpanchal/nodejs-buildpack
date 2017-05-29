043c797666d78651a3ab32a61654445c173333b3
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-29 16:55:22 -0400

    qwe

--- lib/clamav.sh
+++ lib/clamav.sh
@@ -62,3 +62,14 @@ setup_clamav(){
 
 }
 
+
+create_symbol_link(){
+   local build_dir=${1:-}
+   ln -s /home/vcap/app/clamav/lib/libclamav.so.7.1.1 $build_dir/clamav/lib/libclamav.so 
+   ln -s /home/vcap/app/clamav/lib/libclamav.so.7.1.1 $build_dir/clamav/lib/libclamav.so.7 
+   ln -s /home/vcap/app/clamav/lib/libclamunrar_iface.so.7.1.1 $build_dir/clamav/lib/ibclamunrar_iface.so
+   ln -s /home/vcap/app/clamav/lib/libclamunrar_iface.so.7.1.1 $build_dir/clamav/lib/libclamunrar_iface.so.7
+   ln -s /home/vcap/app/clamav/lib/libclamunrar.so.7.1.1  $build_dir/clamav/lib/libclamunrar.so.7
+   ln -s /home/vcap/app/clamav/lib/libclamunrar.so.7.1.1  $build_dir/clamav/lib/libclamunrar.so
+
+}
