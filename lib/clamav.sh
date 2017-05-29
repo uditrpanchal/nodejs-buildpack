ace988b3acc4f1f6e4efb36b9a2672800ca0cba7
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-29 10:09:54 -0400

    logfile

--- lib/clamav.sh
+++ lib/clamav.sh
@@ -1,7 +1,10 @@
 
 source_property(){
-
-    source clamav.properties
+    echo "getting version of clamav and llvm from properties"
+    if [ -f clamav.properties ]
+    then
+        source clamav.properties
+    if
     if [ -z $CLAMAV_V ]
     then
        export CLAMAV_V=0.99.2
@@ -16,6 +19,7 @@ source_property(){
 
 
 get_source(){
+    echo "getting clamav source"
     local build_dir=${1:-}
     curl -Lo $build_dir/clamav.tar.gz https://www.clamav.net/downloads/production/clamav-${CLAMAV_V}.tar.gz
     tar xvf $build_dir/clamav.tar.gz -C $build_dir > /dev/null
@@ -24,28 +28,33 @@ get_source(){
 
 
 get_llvm(){
+       echo "getting llvm source"
        local build_dir=${1:-}
        curl -o $build_dir/llvm.tar.xz http://releases.llvm.org/3.6.0/clang+llvm-${LLVM_V}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
        tar xvf $build_dir/llvm.tar.xz -C $build_dir/ /dev/null
 }
 
 configure(){
+     echo "configre clamav"
     local build_dir=${1:-}
     $build_dir/clamav-${CLAMAV_V}/configure --with-user=vcap --prefix=/home/vcap/app/clamav --disable-clamav --with-system-llvm=$build_dir/clang+llvm-3.6.0-x86_64-linux-gnu/bin/llvm-config
 
 }
 
 compile(){
+      echo "compile clamav"
      local build_dir=${1:-}
      make -C $build_dir/clamav_${CLAMAV_V}
 }
 
 install(){
+    echo "install clamav"
     local build_dir=${1:-}
     make  -C $build_dir/clamav_${CLAMAV_V} install
 }
 
 get_clamav_cvds(){
+    echo " get cvds"
     local build_dir=${1:-}
     mkdir -p $build_dir/clamav/share/clamav/
     wget -O $build_dir/clamav/share/clamav/main.cvd http://database.clamav.net/main.cvd
@@ -54,6 +63,7 @@ get_clamav_cvds(){
 }
 
 create_symbol_link(){
+    echo "create symbolic link for libs"
    local build_dir=${1:-}
    ln -s /home/vcap/app/clamav/lib/libclamav.so.7.1.1 $build_dir/clamav/lib/libclamav.so 
    ln -s /home/vcap/app/clamav/lib/libclamav.so.7.1.1 $build_dir/clamav/lib/libclamav.so.7 
