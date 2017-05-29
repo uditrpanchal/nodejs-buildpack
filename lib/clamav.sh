971d256228e7fb9b6686b76a3f067de2def87645
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-29 10:25:35 -0400

    qweq

--- lib/clamav.sh
+++ lib/clamav.sh
@@ -2,7 +2,7 @@
 get_source(){
     echo "getting clamav source"
     local build_dir=${1:-}
-    curl -Lo $build_dir/clamav.tar.gz https://www.clamav.net/downloads/production/clamav-${CLAMAV_V}.tar.gz
+    curl -Lo $build_dir/clamav.tar.gz https://www.clamav.net/downloads/production/clamav-0.99.2.tar.gz
     tar xvf $build_dir/clamav.tar.gz -C $build_dir > /dev/null
 }
 
@@ -11,27 +11,27 @@ get_source(){
 get_llvm(){
        echo "getting llvm source"
        local build_dir=${1:-}
-       curl -o $build_dir/llvm.tar.xz http://releases.llvm.org/3.6.0/clang+llvm-${LLVM_V}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+       curl -o $build_dir/llvm.tar.xz http://releases.llvm.org/3.6.0/clang+llvm-3.6.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
        tar xvf $build_dir/llvm.tar.xz -C $build_dir/ /dev/null
 }
 
 configure(){
      echo "configre clamav"
     local build_dir=${1:-}
-    $build_dir/clamav-${CLAMAV_V}/configure --with-user=vcap --prefix=/home/vcap/app/clamav --disable-clamav --with-system-llvm=$build_dir/clang+llvm-3.6.0-x86_64-linux-gnu/bin/llvm-config
+    $build_dir/clamav-0.99.2/configure --with-user=vcap --prefix=/home/vcap/app/clamav --disable-clamav --with-system-llvm=$build_dir/clang+llvm-3.6.0-x86_64-linux-gnu/bin/llvm-config
 
 }
 
 compile(){
       echo "compile clamav"
      local build_dir=${1:-}
-     make -C $build_dir/clamav_${CLAMAV_V}
+     make -C $build_dir/clamav_0.99.2
 }
 
 install(){
     echo "install clamav"
     local build_dir=${1:-}
-    make  -C $build_dir/clamav_${CLAMAV_V} install
+    make  -C $build_dir/clamav_0.99.2 install
 }
 
 get_clamav_cvds(){
