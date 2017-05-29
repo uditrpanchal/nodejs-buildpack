1f893736b3eeb3c278e2ac42dd50c57b4f4a9067
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-29 11:40:44 -0400

    into one function

--- lib/clamav.sh
+++ lib/clamav.sh
@@ -1,69 +1,40 @@
 
-get_source(){
-    echo "getting clamav source"
+install_clamav(){
+    echo "pre-install"
     local build_dir=${1:-}
     local old_dir=$(pwd)
     cd $build_dir
+
+    echo "getting clamav source"
     curl --silent -Lo clamav.tar.gz https://www.clamav.net/downloads/production/clamav-0.99.2.tar.gz 
     tar xvf clamav.tar.gz > /dev/null
-    cd $old_dir
-}
 
-
-
-get_llvm(){
-       echo "getting llvm source"
-       local build_dir=${1:-}
-       local old_dir=$(pwd)
-       cd $build_dir
-       curl --silent -o llvm.tar.xz http://releases.llvm.org/3.6.0/clang+llvm-3.6.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz 
-       tar xvf llvm.tar.xz  > /dev/null
-       cd $old_dir
-}
-
-configure(){
-     echo "configre clamav"
-    local build_dir=${1:-}
-     local old_dir=$(pwd)
+    echo "getting llvm source"
+    curl --silent -o llvm.tar.xz http://releases.llvm.org/3.6.0/clang+llvm-3.6.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz 
+    tar xvf llvm.tar.xz  > /dev/null
+      
+    echo "configre clamav"
     cd $build_dir/clamav-0.99.2
     ./configure --with-user=vcap --prefix=/home/vcap/clamav --disable-clamav --with-system-llvm=$build_dir/clang+llvm-3.6.0-x86_64-linux-gnu/bin/llvm-config  > /dev/null
-    cd $old_dir
-}
 
-compile(){
-      echo "compile clamav"
-     local build_dir=${1:-}
-     local old_dir=$(pwd)
-     cd $build_dir/clamav-0.99.2
-     make
-     cd $old_dir
-}
+    echo "compile clamav"
+    make >  /dev/null
 
-install(){
     echo "install clamav"
-    local build_dir=${1:-}
-    local old_dir=$(pwd)
-    cd $build_dir/clamav-0.99.2 
-    make install
-    cd $old_dir
-}
+    make install /dev/null
 
-clean(){
-      local build_dir=${1:-}
-    local old_dir=$(pwd)
-    cd $build_dir
+    echo "post-install"
+    cd $old_dir
+
+    echo "cleanning clamav residue"
     rm clamav.tar.gz
     rm -rf clamav-0.99.2
     rm llvm.tar.xz
     rm -rf clang+llvm-3.6.0-x86_64-linux-gnu
-    cd old_dir
-}
-get_clamav_cvds(){
-    echo " get cvds"
-    local build_dir=${1:-}
+
+    echo " makeing dir for cvds"
     mkdir -p  /home/vcap/clamav/share/clamav/
-    wget -O  /home/vcap/clamav/share/clamav/main.cvd http://database.clamav.net/main.cvd
-    wget -O  /home/vcap/clamav/share/clamav/daily.cvd http://database.clamav.net/daily.cvd
-    wget -O  /home/vcap/clamav/share/clamav/bytecode.cvd http://database.clamav.net/bytecode.cvd
+
+
 }
 
