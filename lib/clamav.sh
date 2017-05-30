b9aed067666beb342300817c993633129bdca233
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-29 21:16:30 -0400

    wq

--- lib/clamav.sh
+++ lib/clamav.sh
@@ -23,46 +23,44 @@ setup_clamav(){
     echo "install clamav"
     make install > /dev/null
 
-
-    echo "moving clamav to proper dir"
-    mv  $HOME/app/clamav $build_dir/clamav 
-
     echo " making dir for cvds"
-    mkdir -p $build_dir/clamav/share/clamav/
+    mkdir -p $HOME/app/clamav/share/clamav/
 
     echo "post-install"
     cd $old_dir
+
+    echo "config freshclam and clam daemon"
+    if [ -f $build_dir/freshclam.conf ]
+    then
+        mv $build_dir/freshclam.conf $HOME/app/clamav/etc/freshclam.conf
+    else
+        mv $build_dir/clamav/etc/freshclam.conf.sample  $HOME/app/clamav/etc/freshclam.conf
+        sed -i 's/^Foreground .*$/Foreground true/g'  $HOME/app/clamav/etc/freshclam.conf
+        sed -i 's/^Example *$//g'   $HOME/app/clamav/etc/freshclam.conf
+    fi
+
+    if [ -f $build_dir/clamd.conf ]
+    then
+        mv $build_dir/clamd.conf  $HOME/app/clamav/etc/clamd.conf
+    else
+        mv $build_dir/clamav/etc/clamd.conf.sample  $HOME/app/clamav/etc/clamd.conf
+        echo "TCPSocket 3310" >>  $HOME/app/clamav/etc/clamd.conf
+        sed -i 's/^Foreground .*$/Foreground true/g'  $HOME/app/clamav/etc/clamd.conf
+        sed -i 's/^Example *$//g'  $HOME/app/clamav/etc/clamd.conf
+    fi
+
+
+    echo "Getting virus database using freshclam"
+     $HOME/app/clamav/bin/freshclam
+
+    echo "move clamav to build directory"
+    mv  $HOME/app/clamav $build_dir
 
     echo "cleanning clamav residue"
     rm $build_dir/clamav.tar.gz
     rm -rf $build_dir/clamav-0.99.2
     rm $build_dir/llvm.tar.xz
     rm -rf $build_dir/clang+llvm-3.6.0-x86_64-linux-gnu
-
-    echo "config freshclam and clam daemon"
-    if [ -f $build_dir/freshclam.conf ]
-    then
-        mv $build_dir/freshclam.conf $build_dir/clamav/etc/freshclam.conf
-    else
-        mv $build_dir/clamav/etc/freshclam.conf.sample $build_dir/clamav/etc/freshclam.conf
-        sed -i 's/^Foreground .*$/Foreground true/g' $build_dir/clamav/etc/freshclam.conf
-        sed -i 's/^Example *$//g'  $build_dir/clamav/etc/freshclam.conf
-    fi
-
-    if [ -f $build_dir/clamd.conf ]
-    then
-        mv $build_dir/clamd.conf $build_dir/clamav/etc/clamd.conf
-        sed -i
-    else
-        mv $build_dir/clamav/etc/clamd.conf.sample $build_dir/clamav/etc/clamd.conf
-        echo "TCPSocket 3310" >> $build_dir/clamav/etc/clamd.conf
-        sed -i 's/^Foreground .*$/Foreground true/g' $build_dir/clamav/etc/clamd.conf
-        sed -i 's/^Example *$//g' $build_dir/clamav/etc/clamd.conf
-    fi
-
-
-    echo "Getting virus database using freshclam"
-    $build_dir/clamav/bin/freshclam
 
 }
 
